95fe2b8a5974e4a42dc749c6b82852abe209f883
Luizuks <144447202+Luizuks@users.noreply.github.com>
2023-09-15 12:10:28 +0300

    Add files via upload

---
+++ izvade1_emils_luize.py
@@ -0,0 +1,13 @@
+#Emīls Sermulis
+print("sekos tukša rinda")
+print("\t")
+print("Visi vārdi vienā rindā")
+print("Visi " ,end="")
+print("Vārdi " ,end="")
+print("Vienā " ,end="")
+print("Rindā ")
+print("No zvaigznītēm * veidots trijstūris")
+
+print("  *")
+print(" * *")
+print("*****")
---
+++ izvade1_emils_luize2.py
@@ -0,0 +1,5 @@
+# Emīls Sermulis
+print('Lugas "Zelta zirgs" autors ir Rainis')
+print("\t10 \t20 \t30")
+print("A","\n\t B","\n\t\tC","\nD")
+
